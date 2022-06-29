c194c3357aef3706dd5ba30d21997b22bff72db7
munachisonwadike <22077758+munachisonwadike@users.noreply.github.com>
2022-06-29 10:08:30 +0000

    folder renaming for wss and added folder for TA

--- hera/hera-example.py
+++ hera/hera-example.py
@@ -13,16 +13,16 @@ while the second submits a workflow which throws an error; this python file is m
 Copy pasted the Hera hellow world example found in the link below, and added in token
 https://github.com/argoproj-labs/hera-workflows/blob/69a996731ee0c7d10e12838b7e9e5d1fa3540dc4/examples/hello_hera.py
 """
-from hera import Task, Workflow, WorkflowService
-def hello():
-    print('Hello, Hera!')
+# from hera import Task, Workflow, WorkflowService
+# def hello():
+#     print('Hello, Hera!')
 
-token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBOaDJoR0hVS25meFhHMDJVNDVYZkFkekNTVHg1a2JFUk5fc0VMT2ZwaHMifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJhcmdvIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImFyZ28tdG9rZW4teDQ5MnoiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiYXJnbyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6Ijg5YTRjMzk4LTM4ZWUtNDhkYi1iYjcwLWFjNjhkMjM3YTdmNyIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDphcmdvOmFyZ28ifQ.Z4YugHjkRdGrfl9oA_BrgTktnlyvbgAgyYAasxngpL-dV9FWi94CtTS6axLx1SpL8umOHI56GqTQhFBadbt_cbWNgRQXe7mv5EHGPBKSvdadYUfeNW655MZsI4Y_8ov_-3uQ-spvIYEXADYelwHHSh6dvomG4_TrOk9Jk5N5quc4zjCecR8jLpcKHLSsen1yDTnLjgSUPBjmjjNkvAaPE-Maphv80rwALZc6AOuZZlYUF4dJamGNptPqylyTKh8enzJCKltxI_UTra8-BiomasrsluOWBJXMR91YHvgyRAAko_C8fTcwWE04hwKN9qN_69UNLOZNRrjGl529uhppGg"
-ws = WorkflowService(host='https://localhost:2746/', token=token, verify_ssl=False)
-w = Workflow('hello-hera', service=ws, namespace="argo")
-t = Task('t', hello)
-w.add_task(t)
-w.create()
+# token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBOaDJoR0hVS25meFhHMDJVNDVYZkFkekNTVHg1a2JFUk5fc0VMT2ZwaHMifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJhcmdvIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImFyZ28tdG9rZW4teDQ5MnoiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiYXJnbyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6Ijg5YTRjMzk4LTM4ZWUtNDhkYi1iYjcwLWFjNjhkMjM3YTdmNyIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDphcmdvOmFyZ28ifQ.Z4YugHjkRdGrfl9oA_BrgTktnlyvbgAgyYAasxngpL-dV9FWi94CtTS6axLx1SpL8umOHI56GqTQhFBadbt_cbWNgRQXe7mv5EHGPBKSvdadYUfeNW655MZsI4Y_8ov_-3uQ-spvIYEXADYelwHHSh6dvomG4_TrOk9Jk5N5quc4zjCecR8jLpcKHLSsen1yDTnLjgSUPBjmjjNkvAaPE-Maphv80rwALZc6AOuZZlYUF4dJamGNptPqylyTKh8enzJCKltxI_UTra8-BiomasrsluOWBJXMR91YHvgyRAAko_C8fTcwWE04hwKN9qN_69UNLOZNRrjGl529uhppGg"
+# ws = WorkflowService(host='https://localhost:2746/', token=token, verify_ssl=False)
+# w = Workflow('hello-hera', service=ws, namespace="argo")
+# t = Task('t', hello)
+# w.add_task(t)
+# w.create()
 
 
 """ 
@@ -47,8 +47,8 @@ token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBOaDJoR0hVS25meFhHMDJVNDVYZkFkekNTVHg1a2J
 ws = WorkflowService(host='https://localhost:2746/', token=token, verify_ssl=False)
 w = Workflow('container', service=ws, namespace="argo")
 
-t = Task('cowsay', say, [{'a':1}], #also tried to pass "None" instead of say function since I just needed docker container commands to run
-	image='docker/whalesay', 
+t = Task('cowsay', say, [{'a':'barackobama'}], #also tried to pass "None" instead of say function since I just needed docker container commands to run
+	image='docker/whalesay', 	
 	command=['cowsay', 'foo'], memoize=Memoize('a', 'cache1', '5m'))
 w.add_task(t)
 w.create()
