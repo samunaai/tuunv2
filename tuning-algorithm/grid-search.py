d49f11816832fcbb4c0a9c2a7232cdebbba66a7a
munachisonwadike <22077758+munachisonwadike@users.noreply.github.com>
2022-06-29 10:11:34 +0000

    added class placeholders for TA

---
+++ tuning-algorithm/grid-search.py
@@ -0,0 +1,10 @@
+
+class TuningAlgorithm()
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def compute_next_eval_point(self, acq_function):
+    self.compute 
+
+  def pass_message_to_wss():
