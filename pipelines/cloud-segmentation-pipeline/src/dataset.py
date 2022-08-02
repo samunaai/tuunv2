784b2d4d389e4967c78351fc6139a6fe3b3e9340
munachisonwadike <22077758+munachisonwadike@users.noreply.github.com>
2022-08-02 22:27:36 +0000

    modded the dataset.py to take CSV files instead of images

--- pipelines/cloud-segmentation-pipeline/src/dataset.py
+++ pipelines/cloud-segmentation-pipeline/src/dataset.py
@@ -14,18 +14,19 @@ class CloudDataset(Dataset):
     ):
         self.df = df
         self.image_folder = os.path.join(get_original_cwd(), cfg.image_in_dir)
-        self.mask_folder = os.path.join(get_original_cwd(), cfg.masks_in_dir)
+        # self.mask_folder = os.path.join(get_original_cwd(), cfg.masks_in_dir)
         self.img_ids = img_ids
         self.transforms = transforms
         self.preprocessing = preprocessing
 
     def __getitem__(self, idx):
         image_name = self.img_ids[idx]
-        mask_path = os.path.join(self.mask_folder, image_name + ".npy")
+        # mask_path = os.path.join(self.mask_folder, image_name + ".npy")
         image_path = os.path.join(self.image_folder, image_name)
         img = cv2.imread(image_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        mask = np.load(mask_path, allow_pickle=False)
+        # mask = np.load(mask_path, allow_pickle=False)
+        mask = make_mask(self.df, image_name)
         augmented = self.transforms(image=img, mask=mask)
         img = augmented["image"]
         mask = augmented["mask"]
